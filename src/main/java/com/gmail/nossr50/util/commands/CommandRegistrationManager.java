c166cc1e1a327ea588cfb11a11eefd6aac94e669
ilightwas <ilightwas@gmail.com>
2025-05-29 18:35:05 -0300

    Add a permission to mmodebug

--- src/main/java/com/gmail/nossr50/util/commands/CommandRegistrationManager.java
+++ src/main/java/com/gmail/nossr50/util/commands/CommandRegistrationManager.java
@@ -116,7 +116,7 @@ public final class CommandRegistrationManager {
     private static void registerMmoDebugCommand() {
         PluginCommand command = mcMMO.p.getCommand("mmodebug");
         command.setDescription(LocaleLoader.getString("Commands.Description.mmodebug"));
-        command.setPermission(null); //No perm required to save support headaches
+        command.setPermission("mcmmo.commands.mmodebug");
         command.setPermissionMessage(permissionsMessage);
         command.setUsage(LocaleLoader.getString("Commands.Usage.0", "mmodebug"));
         command.setExecutor(new PlayerDebugCommand());
